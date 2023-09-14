d1a579dc32e4c6d940acd133a49ac7860a31ef15
Emin <eminttufan@gmail.com>
2023-09-15 02:47:54 +0300

    initial commit

---
+++ src/test/java/com/emintufan/orderservice/CaseStudyOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emintufan.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
